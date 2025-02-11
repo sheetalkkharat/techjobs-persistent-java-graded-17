a8e403a36e8b308f0aa931da0a07ae84ce906608
sheetalkkharat <sheetalkharatk@gmail.com>
2025-02-11 17:42:40 -0600

    Task 3 & 4

--- queries.sql
+++ queries.sql
@@ -1,16 +1,22 @@
 --Part 1
+CREATE TABLE job (
+    INTEGER id,
+    VARCHAR(255) employer,
+    VARCHAR(255) name,
+    VARCHAR(255) skills
+);
 
---CREATE TABLE Job (
---    id INT AUTO_INCREMENT PRIMARY KEY,
---    name VARCHAR(255) NOT NULL,
---    employer VARCHAR(255) NOT NULL,
---    skills VARCHAR(255) NOT NULL
---);
 --Part 2
---    SELECT name  FROM employer WHERE location = "St. Louis City";
-
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 --Part 3
---DROP TABLE Job;
+DROP TABLE job;
 
 --Part 4
---SELECT * FROM skill LEFT JOIN job_skills ON skill.id = job_skills.skills_id WHERE job_skills.jobs_id IS NOT NULL ORDER BY name ASC;
+
+SELECT *
+FROM skill
+LEFT JOIN job_skills ON skill_id = job_skills.skills_id
+WHERE job_skills.jobs_id IS NOT NULL
+ORDER BY name ASC;
